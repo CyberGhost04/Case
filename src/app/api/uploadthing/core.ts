d443f0b330dedaf8edb23c53196c813cc664941e
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-06-28 20:41:01 +0530

    Prisma integrated

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,22 +1,49 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { z } from "zod";
- 
+import sharp from 'sharp'
+import { db } from "@/db";
+
 const f = createUploadthing();
- 
+
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-  .input(z.object({
-    configId : z.string().optional()
-  }))
+    .input(z.object({
+      configId: z.string().optional()
+    }))
     .middleware(async ({ input }) => {
-        return {input}
+      return { input }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-        const {configId} = metadata.input;
+
+      const { configId } = metadata.input;
+
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      // using Prisma from this point onwards
+
+      if (!configId) {
+
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
+
+        return {configId : configuration.id}
+
+      }
+
+
       return { configId };
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
